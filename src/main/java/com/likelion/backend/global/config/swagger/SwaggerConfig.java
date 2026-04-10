cb4fae9e9a6199bb2d2f1631c644b335e53c6ccf
Hyunbin Kim <taque88@naver.com>
2026-04-11 01:17:33 +0900

    feat(swagger): 스웨거 설명 변경

--- src/main/java/com/likelion/backend/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/likelion/backend/global/config/swagger/SwaggerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title = "멋사 충북대 백엔드 세션 API", description = "멋쟁이사자처럼 충북대 백엔드 세션 API입니다.",
+        info = @Info(title = "멋사 충북대 백엔드 세션 API", description = "멋쟁이사자처럼 충북대 백엔드 세션 베이스 API입니다.",
                 version = "1.0.0"),
         servers = {
                 @Server(url = "http://localhost:8080", description = "로컬 서버"),
