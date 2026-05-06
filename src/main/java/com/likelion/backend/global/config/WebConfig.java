91e43c85a8438b9ac636c23eb9b616d9f24cdcb1
yeontteo <yszzivs@gmail.com>
2026-05-06 17:52:35 +0900

    week4 : HTTP 요청 로깅, articla API 실행 시간 측정 구현

---
+++ src/main/java/com/likelion/backend/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.likelion.backend.global.config;
+
+import com.likelion.backend.global.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
